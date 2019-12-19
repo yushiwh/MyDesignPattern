d0b65033ce8b01197356f8928b670c8d1f6adc8f
nick <yushiwh@126.com>
2019-12-19 11:47:17 +0800

    我的设计模式

--- src/main/java/com/ys/Application.java
+++ src/main/java/com/ys/Application.java
@@ -5,12 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+/**
+ * @author Administrator
+ */
 @SpringBootApplication
 public class Application {
-
-
     public static void main(String[] args) {
-
         SpringApplication.run(Application.class, args);
     }
 }
