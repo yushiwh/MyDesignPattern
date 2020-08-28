59cdbdf8cf8c5a63a15e23f562d417a4e2b001b6
nick <94149094@qq.com>
2020-08-28 09:45:01 +0800

    增加自己写的工厂模式

--- src/main/java/com/ys/designpattern/createpattern/factory/cxgc/RoujiaMo.java
+++ src/main/java/com/ys/designpattern/createpattern/factory/cxgc/RoujiaMo.java
@@ -1,11 +1,12 @@
 package com.ys.designpattern.createpattern.factory.cxgc;
 
-import android.util.Log;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by jingbin on 2016/10/22.
  */
-
+@Slf4j
 public abstract class RoujiaMo {
 
     protected String name;
@@ -17,20 +18,20 @@ public abstract class RoujiaMo {
         Meet meet = roujiaMoYLFactory.creatMeet();
         YuanLiao yuanLiao = roujiaMoYLFactory.creatYuanLiao();
 
-        Log.e("---RoujiaMo:", "使用官方的原料 ---" + name + ": 揉面-剁肉-完成准备工作 yuanLiao:"+meet+"yuanLiao:"+yuanLiao);
+        log.info("---RoujiaMo:", "使用官方的原料 ---" + name + ": 揉面-剁肉-完成准备工作 yuanLiao:"+meet+"yuanLiao:"+yuanLiao);
     }
 
     /**
      * 秘制设备--烘烤2分钟
      */
     public void fire() {
-        Log.e("---RoujiaMo:", name + ": 肉夹馍-专用设备-烘烤");
+        log.info("---RoujiaMo:", name + ": 肉夹馍-专用设备-烘烤");
     }
 
     /**
      * 使用你们的专用袋-包装
      */
     public void pack() {
-        Log.e("---RoujiaMo:", name + ": 肉夹馍-专用袋-包装---end");
+        log.info("---RoujiaMo:", name + ": 肉夹馍-专用袋-包装---end");
     }
 }
