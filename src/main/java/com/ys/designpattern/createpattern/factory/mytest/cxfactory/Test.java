53be18cefa03e351ae7dae796addfc9b86b35b0e
nick <94149094@qq.com>
2020-08-31 08:51:27 +0800

    增加自己写的工厂模式

--- src/main/java/com/ys/designpattern/createpattern/factory/mytest/cxfactory/Test.java
+++ src/main/java/com/ys/designpattern/createpattern/factory/mytest/cxfactory/Test.java
@@ -1,8 +1,10 @@
 package com.ys.designpattern.createpattern.factory.mytest.cxfactory;
 
-import com.ys.designpattern.createpattern.factory.mytest.factory.AnimalFactory;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * @author nick
+ */
 @Slf4j
 public class Test {
     public static void main(String[] args) {
